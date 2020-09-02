f286d5afd6ab17f0d2d8b7219b70978088f2a256
18680506315@163.com <18680506315@163.com>
2020-09-02 18:13:14 +0800

    20200902

--- pages/task/task.js
+++ pages/task/task.js
@@ -5,7 +5,8 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        navbarInitTop: 0, //导航栏初始化距顶部的距离
+        isFixedTop: false, //是否固定顶部
     },
 
     /**
@@ -26,7 +27,20 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
+        if (this.data.navbarInitTop == 0) {
+    
+          //获取节点距离顶部的距离
+          wx.createSelectorQuery().select('#navbar').boundingClientRect((rect)=> {
+            if (rect && rect.top > 0) {
+              var navbarInitTop = parseInt(rect.top);
+              this.setData({
+                navbarInitTop: navbarInitTop
+              });
+              console.log(this.data.navbarInitTop);
+            }
+          }).exec();
+    
+        }
     },
 
     /**
@@ -61,6 +75,22 @@ Page({
      * 用户点击右上角分享
      */
     onShareAppMessage: function () {
+
+    },
+    onPageScroll(e) {
+        var that = this;
+        var scrollTop = parseInt(e.scrollTop); //滚动条距离顶部高度
+    
+        //判断'滚动条'滚动的距离 和 '元素在初始时'距顶部的距离进行判断
+        var isSatisfy = scrollTop >= that.data.navbarInitTop ? true : false;
+        //为了防止不停的setData, 这儿做了一个等式判断。 只有处于吸顶的临界值才会不相等
+        if (that.data.isFixedTop === isSatisfy) {
+          return false;
+        }
+    
+        that.setData({
+            isFixedTop: isSatisfy
+        });
 
     }
 })
---
+++ pages/worksDetail/worksDetail.js
@@ -0,0 +1,95 @@
+// pages/worksDetail/worksDetail.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        currentPhotoIndex:1,
+        obj:{
+            imgList:["//img14.360buyimg.com/pop/jfs/t1/114735/33/2427/35818/5ea17d3aE7018d774/acf7950db4ef2c51.jpg",
+            "//img14.360buyimg.com/pop/jfs/t1/88961/36/19902/34141/5ea17d3aEf82710e9/6465b378faf4ea0b.jpg",
+            "//img14.360buyimg.com/pop/jfs/t1/111272/19/2307/93015/5ea17d3aEb521c837/7b9ac8fb948993f0.jpg",
+            "//img14.360buyimg.com/pop/jfs/t1/110316/1/13554/178993/5ea17d3aE88af5e39/1e6dfb8dfb8259f6.jpg",
+            "//img14.360buyimg.com/pop/jfs/t1/119502/34/1004/98273/5ea17d49Ed74b4fa5/49e7bdfc7ab97813.png"]
+        }
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    },
+    //切换图片
+    changeSwiper(e) {
+        let currentPhotoIndex = e.detail.current + 1
+        this.setData({
+            currentPhotoIndex
+        })
+
+    },
+    // 点击图片
+    tapBanner(e) {
+        let current = 'http:' + e.currentTarget.dataset.item
+
+        let urls = []
+        for (let item of this.data.obj.imgList) {
+            urls.push('http:' + item)
+        }
+
+        wx.previewImage({
+            current,
+            urls,
+        })
+    },
+})
